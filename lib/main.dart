171da020c272c3cfa1e181beb1ec0eddd3b27726
marcusmvfa <marcuspa3@gmail.com>
2020-11-09 09:11:54 -0300

    Adicionado a tela de onBoardingScreen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:natuvida_flutter/Home/onboarding_screen.dart';
 import 'package:natuvida_flutter/Postagem/perguntas.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'Home/home.dart';
 import 'Postagem/finalizar.dart';
 import 'Postagem/postagem.dart';
 import 'SobreNos/sobre_nos.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var page;
+
+  bool login = prefs.getBool("isLogged");
+
+  if (login == null) {
+    login = false;
+  }
+
+  page = login == false ? OnboardingScreen() : Home();
+  runApp(MyApp(page: page));
 }
 
 class MyApp extends StatelessWidget {
+  var page;
+  MyApp({Key key, this.page}) : super(key: key);
+
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Natuvida',
       initialRoute: '/',
       routes: {
         // '/': (context) => Home(),
+        '/home': (context) => Home(),
         '/postagem': (context) => Postagem(),
         '/perguntas': (context) => Perguntas(),
         '/finalizar': (context) => Finalizar(),
@@ -28,7 +47,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Home(),
+      home: page
     );
   }
 }
